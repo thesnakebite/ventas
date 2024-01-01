ba0693c3c13fbe67c888785d4fb8abd9a52b2fa2
Álvaro Navarro <alvaro.navarro@manifiesto.biz>
2024-01-01 19:35:20 +0100

    Product creation, model, migration and factory

---
+++ database/migrations/2024_01_01_180824_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->decimal('purchase_price', 10, 2)->nullable();
+            $table->decimal('sale_price', 10, 2);
+            $table->unsignedInteger('stock');
+            $table->unsignedInteger('minimum_stock')->nullable();
+            $table->string('barcode')->nullable();
+            $table->date('due date')->nullable();
+            $table->boolean('active')->default(true);
+            $table->foreignId('category_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
