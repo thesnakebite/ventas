0ffa7f9dbaf9a42d0f133e14dfdb263749d4a277
alvaro.navarro <alvaro.navarro@manifiesto.biz>
2024-01-04 15:04:18 +0100

    See category: create component part 2

--- app/Livewire/Category/CategoryShow.php
+++ app/Livewire/Category/CategoryShow.php
@@ -1,14 +1,17 @@
 <?php
 
 namespace App\Livewire\Category;
-use Livewire\Attributes\Title;
-
-
 use Livewire\Component;
+
+
+use App\Models\Category;
+use Livewire\Attributes\Title;
 
 #[Title('Ver categoría')]
 class CategoryShow extends Component
 {
+    public Category $category;
+
     public function render()
     {
         return view('livewire.category.category-show');
--- resources/views/livewire/category/category-show.blade.php
+++ resources/views/livewire/category/category-show.blade.php
@@ -1,3 +1,13 @@
 <div>
-    <p>Categoría</p>
+    <x-card cardTitle="Detalles categoría" 
+    >
+        <x-slot:cardTools>
+            <a href="{{ route('categories') }}" class="btn btn-primary">
+                <i class="fas fa-arrow-circle-left mr-1"></i>
+                Regresar
+            </a>
+        </x-slot>
+        
+        @dump($category)
+    </x-card>
 </div>
