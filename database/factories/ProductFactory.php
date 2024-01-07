fca2951ea6e21a164fe99f2dc03ae9ddbb3c6baa
Álvaro Navarro <alvaro.navarro@manifiesto.biz>
2024-01-07 19:25:44 +0100

    Seeders products

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -17,7 +17,15 @@ class ProductFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->unique()->word(3, true),
+            'description' => $this->faker->sentence(),
+            'sale_price' => $this->faker->randomNumber(3, true),
+            'purchase_price' => $this->faker->randomNumber(4, true),
+            'stock' => $this->faker->randomNumber(3, true),
+            'minimum_stock' => $this->faker->randomNumber(2, true),
+            'barcode' => $this->faker->ean13(),
+            'date_expired' => $this->faker->date('Y-m-d'),
+            'category_id' => $this->faker->numberBetween(1, \App\Models\Category::count()),
         ];
     }
 }
---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Product;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use Illuminate\Support\Facades\Storage;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Eliminar imagenes de productos
+        Storage::deleteDirectory('public/products');
+        // Creamos el directorio
+        Storage::makeDirectory('public/products');
+
+        Product::factory(80)
+            ->create()
+            ->each(function (Product $product) {
+                $faker = Faker::create();
+
+                $product->image()->create([
+                    'url' => 'products/' . $faker->image('public/storage/products', 640, 480, 'Product', false)]);
+            });
+    }
+}
